9aef3acc2b05987c6f533f8f1c0888d82d7537f3
baebe <baebe@FELICITY>
2024-10-08 21:27:38 -0400

    oo wee

--- chapter 5/chapter 5.cpp
+++ chapter 5/chapter 5.cpp
@@ -1,14 +1,46 @@
-// chapter 5.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	double start, total(0), days, count, increase;
+
+	do
+	{
+		cout << "If you would like to calculate the daily population increase\n"
+			<< "please enter the starting population of the organism\n"
+			<< "It must be a value more than 1, if not, this message will pop up again\n";
+		cin >> start;
+	} while (start < 2);
+
+	do
+	{
+		cout << "\nHow much would you like the population to increase? please enter a percentage in decimal form\n"
+			<< "It must be a value more than 0 and cannot be a negative\n";
+		cin >> increase;
+	} while (increase <= 0);
+
+	do
+	{
+		cout << "How many days would you like them to repopulate for?\n"
+			<< "Please enter a value more than 0\n";
+		cin >> days;
+	} while (days < 1);
+
+	for (count = 1; count <= days; count++)
+
+	{
+		cout << fixed << setprecision(0) << right;
+		cout << "your population for day " << count << " is " << start << endl;
+		start = (start * increase) + start;
+		total += start;
+	}
+	cout << "\n the total population is " << total;
+
+	return 0;
 
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+}
