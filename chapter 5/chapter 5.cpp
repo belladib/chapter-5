0a205f45e6ec54ff48d47cc500e54af86ec01441
baebe <baebe@FELICITY>
2024-10-15 18:39:22 -0400

    i give up

--- chapter 5/chapter 5.cpp
+++ chapter 5/chapter 5.cpp
@@ -1,87 +1,77 @@
-//for sorting names
 #include <iomanip>
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <vector>
-#include <algorithm> // For sorting
+#include <algorithm> // Include for sorting
 using namespace std;
 
-int main()
-{
+int main() {
     string name;
     int menu, count, number;
+    vector<string> names; // Use a vector to store names
     ofstream outputFile;
     ifstream readFile;
-    vector<string> names; // Vector to store names
 
-    outputFile.open("LineUp.txt", ios::app); // Open in append mode
+    outputFile.open("LineUp.txt");
 
-    do
-    {
-        cout << "\nChoose what you would like to do:\n"
-            << "1. Enter names of students\n"
-            << "2. Display names of students\n"
-            << "3. Quit program\n";
-        cin >> menu;
+    cout << "Choose what you would like to do:\n"
+        << "1. Enter names of students\n"
+        << "2. Display names of students\n"
+        << "3. Quit program\n";
+    cin >> menu;
 
-        switch (menu)
-        {
+    do {
+        switch (menu) {
         case 1:
-            cout << "How many students are in your class? ";
+            cout << "How many students are in your class?\n";
             cin >> number;
-
-            // Validate the number of students
-            if (number < 1) {
-                cout << "Please enter a valid number of students.\n";
-                break;
-            }
-
-            for (count = 0; count < number; count++)
-            {
-                cout << "Please enter the name of student " << (count + 1) << ": ";
+            for (count = 0; count < number; count++) {
+                cout << "Please enter the name of the student:\n";
                 cin >> name;
-                outputFile << name << endl; // Write the name to the file
-                names.push_back(name); // Store the name in the vector
+                outputFile << name << endl; // Write name to file
             }
             break;
 
         case 2:
             outputFile.close(); // Close the output file before reading
             readFile.open("LineUp.txt");
-
-            // Check if the file opened successfully
             if (!readFile) {
-                cout << "Error opening file.\n";
-                return 1;
+                cout << "Error opening file." << endl;
             }
+            else {
+                // Read names from the file into the vector
+                while (getline(readFile, name)) {
+                    names.push_back(name);
+                }
+                readFile.close();
 
-            // Clear the vector and read names from the file
-            names.clear();
-            while (getline(readFile, name))
-                names.push_back(name);
+                // Sort names alphabetically
+                sort(names.begin(), names.end());
 
-            readFile.close(); // Close the input file
-            outputFile.open("LineUp.txt"); // Reopen output file for further writing
-
-            // Sort the names alphabetically
-            sort(names.begin(), names.end());
-
-            // Display sorted names
-            cout << "Names of students (in alphabetical order):\n";
-            for (const auto& n : names)
-                cout << n << endl;
-           
+                // Display sorted names
+                cout << "Names of students in alphabetical order:\n";
+                for (const string& sortedName : names) {
+                    cout << sortedName << endl;
+                }
+            }
+            outputFile.open("LineUp.txt", ios::app); // Reopen for further entries
             break;
 
         case 3:
-            cout << "Exiting program.\n";
-            break;
+            return 0;
 
         default:
-            cout << "Invalid choice. Please try again.\n";
+            cout << "Invalid option. Please try again." << endl;
             break;
         }
+
+        cout << "Choose what you would like to do:\n"
+            << "1. Enter names of students\n"
+            << "2. Display names of students\n"
+            << "3. Quit program\n";
+        cin >> menu;
+
     } while (menu != 3);
 
     outputFile.close(); // Close the output file before exiting
