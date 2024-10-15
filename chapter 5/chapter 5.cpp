88c88e1a9c8fd89eacbe8512fc86bad537f87a9c
baebe <baebe@FELICITY>
2024-10-15 15:16:05 -0400

    help

--- chapter 5/chapter 5.cpp
+++ chapter 5/chapter 5.cpp
@@ -1,14 +1,83 @@
-// chapter 5.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <string>
+#include <fstream>
+using namespace std;
 
+int main()
+{
+    string file, town;
+    int interval, amt, start, pop;
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+    cout << "To show a population bar chart please enter the yearly interval rate\n";
+    cin >> interval;
+    if (interval < 1)
+        return 1;
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    cout << "What is the starting year\n";
+    cin >> start;
+    if (start < 1)
+        return 1;
+
+    cout << "How many intervals would you like there to be\n";
+    cin >> amt;
+    if (amt < 1)
+        return 1;
+
+    cout << "What is the name of the town?\n";
+    cin >> town;
+
+    cout << "What would you like to name the file\n";
+    cin >> file;
+
+    ofstream writefile;
+    writefile.open(file);
+
+    if (writefile)
+    {
+        for (int count = 0; count < amt; count++)
+        {
+            cout << "\nWhat is the population of the town for year " << start << endl;
+            cin >> pop;
+
+            int stars = pop / 1000;
+
+            writefile << start << " " << stars << endl; // Write year and stars
+            start += interval;
+        }
+    }
+    else
+    {
+        cout << "Error opening file";
+        return 1;
+    }
+    writefile.close(); // Close the write file
+
+    ifstream readfile;
+    readfile.open(file);
+
+    if (readfile)
+    {
+        cout << "Opening file " << file << endl;
+        cout << town << " Population Growth:\n"
+            << "Each * represents 1,000 people\n";
+
+        int stars;
+        while (readfile >> start >> stars) // Read year and stars
+        {
+            cout << start << " : ";
+            for (int count = 0; count < stars; count++)
+                cout << "*";
+            cout << endl;
+        }
+    }
+    else
+    {
+        cout << "Error opening file";
+        return 1;
+    }
+
+    readfile.close();
+    return 0;
+}
