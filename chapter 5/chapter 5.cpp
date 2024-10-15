e3da0bce5a70377558d478914f799864a72155c0
baebe <baebe@FELICITY>
2024-10-15 19:03:19 -0400

    :{

--- chapter 5/chapter 5.cpp
+++ chapter 5/chapter 5.cpp
@@ -1,26 +1,55 @@
-#include <iostream>
+/*#include <iostream>
 #include <cmath>
 #include <iomanip>
+//#include <string>
 
 using namespace std;
 
 int main()
 {
+	//string *
+	
+		for (int count = 0; count < 10; count++)
 
-	for (int count = 0; count < 10 ; count++)
+	{
+		int stars = count / 1;
 
-
-
-
-
-
-
-
-
-
-
+		for (int count = 0; count < stars; count++)
+		{cout << "*";
+		cout << endl; }
+	}*/
 
 
+#include <iostream>
+using namespace std;
 
+int main()
+{
+    // Display Pattern A
+    cout << "Pattern A\n";
+    for (int count = 1; count <= 10; count++) {
+        for (int count1 = 1; count1 <= count; count1++) {
+            cout << "+";
+        }
+        cout << endl;
+    }
 
+    // Display Pattern B
+    cout << "\nPattern B\n";
+    for (int count = 10; count >= 1; count--) {
+        for (int count1 = 1; count1 <= count; count1++) {
+            cout << "+";
+        }
+        cout << endl;
+    }
+
+    return 0;
 }
+
+
+
+
+
+
+
+//}
